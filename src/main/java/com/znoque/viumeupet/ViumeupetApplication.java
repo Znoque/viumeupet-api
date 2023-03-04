ffef38de399a973edab9b5bff981afc6af5fc05c
Enoque Teixeira Barbosa <znoque5@gmail.com>
2023-03-04 20:33:43 -0300

    Spring Boot Project

---
+++ src/main/java/com/znoque/viumeupet/ViumeupetApplication.java
@@ -0,0 +1,13 @@
+package com.znoque.viumeupet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViumeupetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViumeupetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/znoque/viumeupet/ViumeupetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.znoque.viumeupet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViumeupetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
